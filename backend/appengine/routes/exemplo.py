6afc0eb1de5bf1f39d6a8dc63a48d12394c2fec0
renzon <renzon@gmail.com>
2014-08-13 08:50:41 -0300

    Final da aula 1

---
+++ backend/appengine/routes/exemplo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from config.template_middleware import TemplateResponse
+from gaecookie.decorator import no_csrf
+
+
+@no_csrf
+def index():
+    return TemplateResponse()
+
