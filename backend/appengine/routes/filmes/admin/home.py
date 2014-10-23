a5eb7ef50842fc72f8fab7e9912beae9eb86952b
renzon <renzon@gmail.com>
2014-10-23 10:52:40 -0200

    Listagem, remoção, e salvamento

--- backend/appengine/routes/filmes/admin/home.py
+++ backend/appengine/routes/filmes/admin/home.py
@@ -14,20 +14,6 @@ def delete(_handler, filme_id):
 
 @no_csrf
 def index():
-    cmd = facade.list_filmes_cmd()
-    filmes = cmd()
-    edit_path = router.to_path(edit)
-    delete_path = router.to_path(delete)
-    short_form = facade.filme_short_form()
-
-    def short_filme_dict(filme):
-        filme_dct = short_form.fill_with_model(filme)
-        filme_dct['edit_path'] = router.to_path(edit_path, filme_dct['id'])
-        filme_dct['delete_path'] = router.to_path(delete_path, filme_dct['id'])
-        return filme_dct
-
-    short_filmes = [short_filme_dict(filme) for filme in filmes]
-    context = {'filmes': short_filmes,
-               'new_path': router.to_path(new)}
+    context = {'new_path': router.to_path(new)}
     return TemplateResponse(context)
 
--- backend/appengine/static/filmes.js
+++ backend/appengine/static/filmes.js
@@ -19,6 +19,40 @@ $(document).ready(function () {
 
     $ajaxGif.hide();
     var $salvarBtn = $('#salvar-btn');
+    var $helpTituloSpan = $('#help-titulo');
+    var $corpoTabela = $('#corpo-tabela');
+
+    var adicionarLinha=function adicionarLinha(filme) {
+        var linha = '<tr id="tr' + filme.id + '"> <td>' + filme.titulo + '</td>' +
+            '<td>' + filme.preco + '</td>' +
+            '<td>' + filme.data + '</td>' +
+            '<td><button id="bt' + filme.id + '" class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash"></i></button>' +
+            '</td> </tr>';
+
+        $corpoTabela.prepend(linha);
+
+        var $linhaHtml = $('#tr' + filme.id);
+
+        $linhaHtml.hide();
+        $linhaHtml.fadeIn();
+        $('#bt' + filme.id).click(function () {
+            $linhaHtml.fadeOut();
+            $.post('/filmes/rest/delete',{'filme_id':filme.id}).success(function(){
+                $linhaHtml.remove();
+            }).error(function(){
+                alert('Remoção não funcionou');
+                $linhaHtml.fadeIn();
+            });
+        });
+    }
+
+    $.get('/filmes/rest').success(function (listaDeFilmes) {
+        for (var i = 0; i < listaDeFilmes.length; i += 1) {
+            adicionarLinha(listaDeFilmes[i]);
+        }
+
+    });
+
     $salvarBtn.click(function () {
         var filme = {titulo: $tituloInput.val(),
             preco: $precoInput.val(),
@@ -28,15 +62,14 @@ $(document).ready(function () {
         $salvarBtn.hide();
         var promessa = $.post('/filmes/rest/save', filme);
         promessa.success(function (filme_do_servidor) {
-            console.log(filme_do_servidor);
+            adicionarLinha(filme_do_servidor);
         });
 
 
         promessa.error(function (erros) {
-            console.log(erros);
             if (erros.responseJSON != null && erros.responseJSON.titulo != null) {
-
                 $tituloDiv.addClass('has-error');
+                $helpTituloSpan.text(erros.responseJSON.titulo);
             }
         });
 
