6c9687297ec53032d0c908552ef7ec47b9785e3b
renzon <renzon@gmail.com>
2014-11-12 07:37:13 -0200

    Salvamento com mensagens de erro

--- backend/appengine/static/filme/js/filme.js
+++ backend/appengine/static/filme/js/filme.js
@@ -6,18 +6,19 @@ filmeModulo.directive('filmeForm', [function () {
         templateUrl: '/static/filme/html/form.html',
         scope: {},
         controller: function ($scope, $http,FilmeAPI) {
-            $scope.filme = {titulo: 'Sonho de Liberdade', preco: '45', data: '02/07/2014'};
+            $scope.filme = {titulo: 'Sonho de Liberdade', preco: '45a', data: '02/07/2014'};
             $scope.executandoSalvamento=false;
-            console.log();
+            $scope.erros={};
+
             $scope.salvar = function () {
                 $scope.executandoSalvamento=true;
+                $scope.erros={};
                 var promessa = FilmeAPI.salvar($scope.filme);
                 promessa.success(function(filme){
-                    console.log(filme);
                     $scope.executandoSalvamento=false;
                 });
                 promessa.error(function(erros){
-                    console.log(erros);
+                    $scope.erros=erros;
                     $scope.executandoSalvamento=false;
                 });
             }
